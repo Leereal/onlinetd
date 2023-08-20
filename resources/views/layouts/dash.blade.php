e05c39abf0971559dd45b2f1c006d4cf7b2be2ce
Leereal <leereal08@ymail.com>
2023-08-20 19:29:50 +0200

    Adjust Tawk.to

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -94,6 +94,20 @@
     <script src="{{ asset('themes/purposeTheme/assets/js/scriptfile.js') }}"></script>
     <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
     </script>
+     </script>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/646a5ed674285f0ec46cc578/1h0vo4u0d';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+        </script>
+        <!--End of Tawk.to Script-->
 
     <script type="text/javascript">
         function googleTranslateElementInit() {
